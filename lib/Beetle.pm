7a3da5bd25fc2bbc1fd4b050b87b7fb7e2d9adb9
Dennis Schoen <dennis.schoen@xing.com>
2010-05-14 11:09:29 +0200

    create Module::Install framework

---
+++ lib/Beetle.pm
@@ -0,0 +1,39 @@
+package Beetle;
+
+use 5.006000;
+
+our $VERSION= '0.01_01';
+
+1;
+__END__
+
+=head1 NAME
+
+Beetle - TODO
+
+=head1 DESCRIPTION
+
+TODO
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 XING AG
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.9 or,
+at your option, any later version of Perl 5 you may have available.
+
+=head1 WARRANTY
+
+This is free software. IT COMES WITHOUT WARRANTY OF ANY KIND.
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Johannes Plunien <johannes.plunien@xing.com>, Dennis Schoen <dennis.schoen@xing.com>
+
+=cut
