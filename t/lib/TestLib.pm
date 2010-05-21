ef888f7250e36d37dc6173a4e78f0e64e0ca7de2
Johannes Plunien <johannes.plunien@xing.com>
2010-05-21 13:29:15 +0200

    Add proper mockup for headers in message test

--- t/lib/TestLib.pm
+++ t/lib/TestLib.pm
@@ -3,21 +3,17 @@ package    # hide from PAUSE
 
 use Moose;
 use Beetle::Message;
-use TestLib::Header;
-
-# def header_with_params(opts = {})
-#   beetle_headers = Beetle::Message.publishing_options(opts)
-#   header = mock("header")
-#   header.stubs(:properties).returns(beetle_headers)
-#   header
-# end
+use Test::MockObject;
 
 sub header_with_params {
     my (%opts) = @_;
 
     my $beetle_headers = Beetle::Message->publishing_options(%opts);
 
-    return TestLib::Header->new( properties => $beetle_headers );
+    my $obj = Test::MockObject->new;
+    $obj->mock( 'properties' => sub { return $beetle_headers } );
+
+    return $obj;
 }
 
 1;
