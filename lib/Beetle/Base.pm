d7a98476457e55da7e7fe47d92501d61ce0cf5e4
Johannes Plunien <johannes.plunien@xing.com>
2010-05-24 13:57:10 +0200

    Add config class

--- lib/Beetle/Base.pm
+++ lib/Beetle/Base.pm
@@ -2,6 +2,13 @@ package Beetle::Base;
 
 use Moose;
 with qw(MooseX::Log::Log4perl);
+use Beetle::Config;
+
+has 'config' => (
+    default => sub { Beetle::Config->new },
+    is      => 'ro',
+    isa     => 'Beetle::Config',
+);
 
 sub BUILD {
     my ($self) = @_;
@@ -11,7 +18,7 @@ sub BUILD {
 sub _setup_logger {
     my ($self) = @_;
 
-    Log::Log4perl->easy_init();
+    Log::Log4perl->easy_init({ file => $self->config->logger });
 }
 
 1;
---
+++ lib/Beetle/Config.pm
@@ -0,0 +1,69 @@
+package Beetle::Config;
+
+use Moose;
+
+has 'logger' => (
+    default       => 'STDERR',
+    documentation => 'default logfile (default: STDERR)',
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+);
+
+has 'gc_threshold' => (
+    default       => 86400 * 3,
+    documentation => 'number of seconds after which keys are removed form the deduplication store (default: 3 days)',
+    is            => 'rw',
+    isa           => 'Int',
+    required      => 1,
+);
+
+has 'redis_hosts' => (
+    default       => 'localhost:6379',
+    documentation => 'the machines where the deduplication store lives (default: localhost:6379)',
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+);
+
+has 'redis_db' => (
+    default       => 4,
+    documentation => 'redis database number to use for the message deduplication store (default: 4)',
+    is            => 'rw',
+    isa           => 'Int',
+    required      => 1,
+);
+
+has 'servers' => (
+    default       => 'localhost:5672',
+    documentation => 'list of amqp servers to use (default: localhost:5672)',
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+);
+
+has 'vhost' => (
+    default       => '/',
+    documentation => 'the virtual host to use on the AMQP servers (default: /)',
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+);
+
+has 'user' => (
+    default       => 'guest',
+    documentation => 'the AMQP user to use when connecting to the AMQP servers (default: guest)',
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+);
+
+has 'password' => (
+    default       => 'guest',
+    documentation => 'the password to use when connectiong to the AMQP servers (default: guest)',
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+);
+
+1;
