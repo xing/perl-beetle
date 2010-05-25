7e536449bf31b81e5de8e77ed3fb5e92c69a9e16
Johannes Plunien <johannes.plunien@xing.com>
2010-05-25 07:06:39 +0200

    Update test.pl

--- examples/test.pl
+++ examples/test.pl
@@ -4,8 +4,16 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use Beetle::DeduplicationStore;
+use Data::Dumper;
+$Data::Dumper::Sortkeys=1;
+
 my $store = Beetle::DeduplicationStore->new( hosts => "127.0.0.1:6379" );
 
 my ( $id, $sfx, $val ) = qw(message_id suffix value);
 $store->set( $id, $sfx, $val );
 warn $store->get( $id, $sfx );
+
+$store->msetnx( "${id}2" => { foo => 'bar', bla => 'fasel' });
+
+my @keys = $store->redis->keys('*');
+warn Dumper \@keys;
