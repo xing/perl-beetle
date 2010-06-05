8d895d8486137a7484cc847d6ed11e614fabe475
Johannes Plunien <johannes.plunien@xing.com>
2010-06-05 15:51:47 +0200

    Add live test example

---
+++ t/09_live_redundant.t
@@ -0,0 +1,60 @@
+use strict;
+use warnings;
+use Test::More;
+
+use FindBin qw( $Bin );
+use lib ( "$Bin/lib", "$Bin/../lib" );
+use TestLib::Live;
+
+BEGIN {
+    use_ok('Beetle::Client');
+}
+
+test_beetle_live(
+    sub {
+        my $ports = shift;
+
+        my $rabbit = sprintf 'localhost:%d localhost:%d', $ports->{rabbit1}, $ports->{rabbit2};
+        my $redis = sprintf 'localhost:%d', $ports->{redis1};
+
+        my $client = Beetle::Client->new(
+            config => {
+                logger      => '/dev/null',
+                redis_hosts => $redis,
+                servers     => $rabbit,
+            }
+        );
+
+        $client->register_queue('testperl');
+        $client->purge('testperl');
+        $client->register_message( testperl => { redundant => 1 } );
+
+        my $N = 3;
+        my $n = 0;
+        for ( 1 .. $N ) {
+            $n += $client->publish( testperl => "Hello$_" );
+        }
+        is( $n, 2 * $N, 'published 6 test messages' );
+
+        my $k = 0;
+
+        $client->register_handler(
+            testperl => sub {
+                $k++;
+            }
+        );
+
+        my $timer = AnyEvent->timer(
+            after => 1,
+            cb    => sub {
+                $client->stop_listening;
+                is( $N, $k, '3 out of the 6 messages got handled (duplicated removed successfully)' );
+            },
+        );
+
+        $client->listen;
+    }
+);
+
+done_testing;
+
---
+++ t/lib/TestLib/Live.pm
@@ -0,0 +1,65 @@
+package    # hide from PAUSE
+  TestLib::Live;
+
+use strict;
+use warnings;
+use FindBin qw( $Bin );
+use Test::More;
+use Test::TCP::Multi;
+use TestLib::Redis ();
+
+use base qw(Exporter);
+our @EXPORT = qw(test_beetle_live);
+
+sub test_beetle_live {
+    my $cb = shift;
+
+    plan skip_all => 'export BEETLE_LIVE_TEST to enable this test' unless $ENV{BEETLE_LIVE_TEST};
+
+    chomp( my $rabbitmq_ctl = `which rabbitmqctl` );
+    unless ( $rabbitmq_ctl && -e $rabbitmq_ctl && -x _ ) {
+        die 'rabbitmqctl not found in your PATH';
+    }
+
+    chomp( my $rabbitmq_server = `which rabbitmq-server` );
+    unless ( $rabbitmq_server && -e $rabbitmq_server && -x _ ) {
+        die 'rabbitmq-server not found in your PATH';
+    }
+
+    chomp( my $redis_server = `which redis-server` );
+    unless ( $redis_server && -e $redis_server && -x _ ) {
+        die 'redis-server not found in your PATH';
+    }
+
+    test_multi_tcp(
+        server1 => sub {
+            my ( $port, $data_hash ) = @_;
+            exec "sudo -n $Bin/script/start_rabbit rabbit1 $port";
+        },
+        server2 => sub {
+            my ( $port, $data_hash ) = @_;
+            exec "sudo -n $Bin/script/start_rabbit rabbit2 $port";
+        },
+        server3 => sub {
+            my ( $port, $data_hash ) = @_;
+            TestLib::Redis::generate_redis_conf($port);
+            exec 'redis-server', 't/redis.conf';
+        },
+
+        client1 => sub {
+            my ($data_hash) = @_;
+            $cb->(
+                {
+                    rabbit1 => $data_hash->{server1}{port},
+                    rabbit2 => $data_hash->{server2}{port},
+                    redis1  => $data_hash->{server3}{port},
+                }
+            );
+            system("sudo $rabbitmq_ctl -n rabbit1 stop");
+            system("sudo $rabbitmq_ctl -n rabbit2 stop");
+            kill 9, $data_hash->{server3}{pid};
+        },
+    );
+}
+
+1;
